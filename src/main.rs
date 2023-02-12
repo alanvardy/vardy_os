ce1acf91e75d41e23fbb6d22b30de19267cb4a3c
Alan Vardy <alan@vardy.cc>
2023-02-11 19:40:28 -0800

    deploy to fly

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,17 @@ use std::net::TcpListener;
 
 use vardy_os::run;
 
+const PROD_ADDR: &str = "0.0.0.0:8080";
+const DEV_ADDR: &str = "127.0.0.1:8000";
+
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    let listener = TcpListener::bind("127.0.0.1:8000").expect("Failed to bind port 8000");
+    let address = if is_prod() { PROD_ADDR } else { DEV_ADDR };
+    let listener = TcpListener::bind(address).expect("Failed to bind port");
     run(listener)?.await
+}
+
+fn is_prod() -> bool {
+    let env = std::env::var("ENV");
+    env == Ok(String::from("prod"))
 }
